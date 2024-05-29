e86b37eb09ea48daf4275da03d37a8212a85e1e0
StefanRuskov <stefanruskov2@abv.bg>
2024-05-29 20:53:06 +0300

    proj final

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
-require(["esri/config", "esri/WebMap", "esri/views/MapView"],
-    function (esriConfig, WebMap, MapView) {
+require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home", "esri/widgets/LayerList", "esri/widgets/BasemapGallery", "esri/widgets/Directions", "esri/layers/RouteLayer", "esri/widgets/ScaleBar", "esri/widgets/Search"],
+    function (esriConfig, WebMap, MapView, Home, LayerList, BasemapGallery, Directions, RouteLayer, ScaleBar, Search) {
 
         esriConfig.apiKey = "AAPK77ebc8a134de4074a160da0e32d0d877onv_cK6xV7_3fD5hbgt8oYIPGcwHVf3SZVZkhweY7eONVEXqwdhRAkgphMmXCg9x"
+
+        const routeLayer = new RouteLayer();
 
         const webmap = new WebMap({
             portalItem: {
@@ -9,8 +11,88 @@ require(["esri/config", "esri/WebMap", "esri/views/MapView"],
             }
         });
 
+        webmap.layers.add(routeLayer);
+
         const view = new MapView({
-        container: "viewDiv",
-        map: webmap
-    })
+            container: "viewDiv",
+            map: webmap
+        });
+
+        const directionsWidget = new Directions({
+            layer: routeLayer,
+            apiKey: "AAPK77ebc8a134de4074a160da0e32d0d877onv_cK6xV7_3fD5hbgt8oYIPGcwHVf3SZVZkhweY7eONVEXqwdhRAkgphMmXCg9x",
+            view
+        });
+
+        view.ui.add(directionsWidget, { position: "bottom-right" });
+
+        const homeBtn = new Home({
+            view,
+        });
+
+        view.ui.add(homeBtn, "top-left");
+
+        const layerList = new LayerList({
+            view,
+        });
+
+        view.ui.add("layer-list-btn", "top-right");
+        view.ui.add(layerList, "top-right");
+        view.ui.add("basemap-gallery-btn", "top-right");
+
+        const basemapGallery = new BasemapGallery({
+            view
+        });
+
+        view.ui.add(basemapGallery, "top-right");
+
+        const scaleBar = new ScaleBar({
+            view
+        });
+        view.ui.add(scaleBar, {
+            position: "bottom-left"
+        });
+
+        const searchWidget = new Search({
+            view
+        });
+        view.ui.add(searchWidget, {
+            position: "top-left",
+            index: 0
+        });
+
+        document.getElementById("layer-list-btn").addEventListener("click", function () {
+            toggleButton("layerList");
+        });
+
+        document.getElementById("basemap-gallery-btn").addEventListener("click", function () {
+            toggleButton("gallery");
+        });
+
+        function toggleButton(element) {
+            if (element == "layerList") {
+            const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+            const currentProp = layerListEl.style.getPropertyValue("display");
+
+            layerListEl.style.setProperty("display", currentProp == "none" ? "block" : "none");
+            } else if (element == "gallery") {
+            const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
+            const currentPropGallery = galleryEl.style.getPropertyValue("display");
+            galleryEl.style.setProperty("display", currentPropGallery == "none" ? "block" : "none");
+            }
+            
+
+
+            
+        };
+
+        
+
+        
+
+        
+
+
+
+
     })
