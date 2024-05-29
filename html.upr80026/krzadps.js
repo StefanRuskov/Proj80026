a95368494ccc611cc45312c9a3d8028a7f92bd68
StefanRuskov <stefanruskov2@abv.bg>
2024-05-29 22:12:56 +0300

    html upr

---
+++ html.upr80026/krzadps.js
@@ -0,0 +1,9 @@
+function sumFirstandLast(arr) {
+let firstEl = input[0];
+let lastEl = arr[arr.length - 1];
+let sum 
+    //let sum = arr[0] + arr.length -1
+console.log(sum);
+}
+
+sumFirstandLast([20, 30, 40]);
---
+++ html.upr80026/test.js
@@ -0,0 +1,5 @@
+
+function Point([arg1,arg2,arg3]){
+}
+
+RectangleArea([8]);
